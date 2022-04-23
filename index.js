9d7d2185769fa4da8a73eb7d0b4b4a1c998128fc
mircogamer2011 <ssm29056@gmail.com>
2022-04-23 15:46:38 +0200

    clear token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] }); //<-- RICORDARSI QUESTO
 
-client.login("OTY3NDA5NjIyMDEwOTcwMTUy.YmP4aw.EtN1yuahEEcl6L8473fNJVCBGA4");
+client.login(process.env.token);
 
 client.on("ready", () => {
     console.log("ONLINE")
